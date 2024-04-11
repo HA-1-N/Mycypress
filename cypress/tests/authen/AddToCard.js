5cb7795d7ea892e365eb7702211a9c0ee9353178
DESKTOP-ESNC61C\FPT <anhnh133@fpt.com>
2024-04-11 18:29:01 +0700

    add xpath

--- cypress/tests/authen/AddToCard.js
+++ cypress/tests/authen/AddToCard.js
@@ -1,4 +1,5 @@
-const { isLoggedIn } = require("./LoginTest");
+const { isLoggedIn, login } = require("./LoginTest");
+require('cypress-xpath')
 
 describe('Add to Cart Test', () => {
     beforeEach(async () => {
@@ -7,16 +8,22 @@ describe('Add to Cart Test', () => {
     });
 
     it('should be able to add to cart', () => {
-        cy.get('.text-center').contains('Add to cart').first().click().then(() => {
-            // Kiểm tra xem đã đăng nhập hay chưa
-            if (isLoggedIn) {
-                // Nếu đã đăng nhập, kiểm tra sản phẩm đã được thêm vào giỏ hàng thành công
-                cy.get('button.btn.btn-outline-light').contains('Cart 1').should('exist');
-            } else {
-                // Nếu chưa đăng nhập, thực hiện đăng nhập trước khi kiểm tra giỏ hàng
-                login('username', 'password');
+        cy.xpath("(//a[text()='Add to cart'])[1]").first().click().then(() => {
+            //Kiểm tra xem đã đăng nhập hay chưa
+            if(cy.xpath("//button[text()='ĐĂNG NHẬP']").should("be.visible"))
+            {
+                login('luucong1008@gmail.com', '10072000');
+            }else{
                 cy.get('.btn.btn-outline-light').contains('Cart 1').should('exist');
             }
+            // if (isLoggedIn) {
+            //     // Nếu đã đăng nhập, kiểm tra sản phẩm đã được thêm vào giỏ hàng thành công
+            //     cy.get('button.btn.btn-outline-light').contains('Cart 1').should('exist');
+            // } else {
+            //     // Nếu chưa đăng nhập, thực hiện đăng nhập trước khi kiểm tra giỏ hàng
+            //     login('username', 'password');
+            //     cy.get('.btn.btn-outline-light').contains('Cart 1').should('exist');
+            // }
         });
         // cy.get('.text-center').contains('Add to cart').first().click().then(() => {
         //     if (isLoggedIn) {
@@ -28,4 +35,12 @@ describe('Add to Cart Test', () => {
         //     cy.get('.btn.btn-outline-light').contains('Cart 1').should('exist');
         // });
     });
+    function login(username, password) {
+        cy.get('input[formcontrolname="username"]').type(username);
+        cy.get('input[formcontrolname="password"]').type(password);
+        cy.xpath("(//button[text()='ĐĂNG NHẬP'])[1]").click();
+        // cy.on("window:alert", (alert) => {
+        //   expect(alert).to.contain(messages);
+        // });
+      }
 });
--- cypress/tests/authen/LoginTest.js
+++ cypress/tests/authen/LoginTest.js
@@ -1,117 +1,117 @@
-let isLoggedIn = false;
+// let isLoggedIn = false;
 
-const LOGIN_CRED = {
-  username: "luucong1008@gmail.com",
-  password: "10072000",
-};
-const messages = {
-  success: "Đăng nhập thành công",
-  wrongUsername: "Tài khoản không hợp lệ",
-  wrongPassword: "Mật khẩu không đúng",
-};
-// Hàm để thực hiện đăng nhập thành công
-function login(username, password) {
-  cy.get('input[formcontrolname="username"]').type(username);
-  cy.get('input[formcontrolname="password"]').type(password);
-  cy.get(".btn.btn-success").click();
-  cy.on("window:alert", (alert) => {
-    expect(alert).to.contain(messages);
-  });
-}
+// const LOGIN_CRED = {
+//   username: "luucong1008@gmail.com",
+//   password: "10072000",
+// };
+// const messages = {
+//   success: "Đăng nhập thành công",
+//   wrongUsername: "Tài khoản không hợp lệ",
+//   wrongPassword: "Mật khẩu không đúng",
+// };
+// // Hàm để thực hiện đăng nhập thành công
+// function login(username, password) {
+//   cy.get('input[formcontrolname="username"]').type(username);
+//   cy.get('input[formcontrolname="password"]').type(password);
+//   cy.xpath("//button[text()='ĐĂNG NHẬP']").click();
+//   cy.on("window:alert", (alert) => {
+//     expect(alert).to.contain(messages);
+//   });
+// }
 
-// Hàm để thực hiện đăng nhập không thành công
-function loginUnsuccessful(username, password, messages) {
-  cy.get('input[formcontrolname="username"]').type(username);
-  cy.get('input[formcontrolname="password"]').type(password);
-  cy.get(".btn.btn-success").click();
-  cy.on("window:alert", (alert) => {
-    expect(alert).to.contain(messages);
-  });
-}
+// // Hàm để thực hiện đăng nhập không thành công
+// function loginUnsuccessful(username, password, messages) {
+//   cy.get('input[formcontrolname="username"]').type(username);
+//   cy.get('input[formcontrolname="password"]').type(password);
+//   cy.get(".btn.btn-success").click();
+//   cy.on("window:alert", (alert) => {
+//     expect(alert).to.contain(messages);
+//   });
+// }
 
-function hidepassword(username, password) {
-  cy.get('input[formcontrolname="password"]').type(`${LOGIN_CRED.password}`);
-  cy.get('input[id="flexCheckChecked"]').check();
-  cy.get('input[formcontrolname="password"]').should(
-    "have.attr",
-    "type",
-    "text"
-  );
-  cy.get('input[id="flexCheckChecked"]').uncheck();
-  cy.get('input[formcontrolname="password"]').should(
-    "have.attr",
-    "type",
-    "password"
-  );
-}
+// function hidepassword(username, password) {
+//   cy.get('input[formcontrolname="password"]').type(`${LOGIN_CRED.password}`);
+//   cy.get('input[id="flexCheckChecked"]').check();
+//   cy.get('input[formcontrolname="password"]').should(
+//     "have.attr",
+//     "type",
+//     "text"
+//   );
+//   cy.get('input[id="flexCheckChecked"]').uncheck();
+//   cy.get('input[formcontrolname="password"]').should(
+//     "have.attr",
+//     "type",
+//     "password"
+//   );
+// }
 
-function forgotPassword(username, password) {
-  cy.visit("/login");
-  cy.get('.row.mt-2.ps-3 a[href="/forgot-password"]')
-    .click()
-    .then(() => {
-      // Thực hiện kiểm tra hoặc hành động tiếp theo ở đây
-      cy.url().should("include", "/forgot-password");
-    });
-}
+// function forgotPassword(username, password) {
+//   cy.visit("/login");
+//   cy.get('.row.mt-2.ps-3 a[href="/forgot-password"]')
+//     .click()
+//     .then(() => {
+//       // Thực hiện kiểm tra hoặc hành động tiếp theo ở đây
+//       cy.url().should("include", "/forgot-password");
+//     });
+// }
 
-describe("Login Test", () => {
-  beforeEach(() => {
-    cy.visit("/login");
-  });
+// describe("Login Test", () => {
+//   beforeEach(() => {
+//     cy.visit("/login");
+//   });
 
-  it("Login success", () => {
-    login(LOGIN_CRED.username, LOGIN_CRED.password, messages.success);
-    isLoggedIn = true; // Đánh dấu đã đăng nhập thành công
-  });
+//   it("Login success", () => {
+//     login(LOGIN_CRED.username, LOGIN_CRED.password, messages.success);
+//     isLoggedIn = true; // Đánh dấu đã đăng nhập thành công
+//   });
 
-  it("should be able to wrong username", () => {
-    loginUnsuccessful(
-      `${LOGIN_CRED.username}_WRONG`,
-      LOGIN_CRED.password,
-      messages.wrongUsername
-    );
-    isLoggedIn = false; // Đánh dấu đã đăng nhập không thành công
-  });
+//   it("should be able to wrong username", () => {
+//     loginUnsuccessful(
+//       `${LOGIN_CRED.username}_WRONG`,
+//       LOGIN_CRED.password,
+//       messages.wrongUsername
+//     );
+//     isLoggedIn = false; // Đánh dấu đã đăng nhập không thành công
+//   });
 
-  it("should be able to wrong password", () => {
-    loginUnsuccessful(
-      LOGIN_CRED.username,
-      `${LOGIN_CRED.password}_WRONG`,
-      messages.wrongPassword
-    );
-    isLoggedIn = false;
-  });
-  it("should be able to not type username", () => {
-    loginUnsuccessful("", LOGIN_CRED.password);
-    cy.get(".btn.btn-success").should("be.disabled");
-    isLoggedIn = false;
-  });
+//   it("should be able to wrong password", () => {
+//     loginUnsuccessful(
+//       LOGIN_CRED.username,
+//       `${LOGIN_CRED.password}_WRONG`,
+//       messages.wrongPassword
+//     );
+//     isLoggedIn = false;
+//   });
+//   it("should be able to not type username", () => {
+//     loginUnsuccessful("", LOGIN_CRED.password);
+//     cy.get(".btn.btn-success").should("be.disabled");
+//     isLoggedIn = false;
+//   });
 
-  it("should be able to not type password", () => {
-    loginUnsuccessful(LOGIN_CRED.username);
-    cy.get(".btn.btn-success").should("be.disabled");
-    isLoggedIn = false;
-  });
+//   it("should be able to not type password", () => {
+//     loginUnsuccessful(LOGIN_CRED.username);
+//     cy.get(".btn.btn-success").should("be.disabled");
+//     isLoggedIn = false;
+//   });
 
-  it("should disable login button when username is whitespace only", () => {
-    loginUnsuccessful("    ", LOGIN_CRED.password, messages.wrongUsername);
-    isLoggedIn = false;
-  });
-  it("should disable login button when username has leading and trailing whitespace", () => {
-    loginUnsuccessful(
-      "   " + LOGIN_CRED.username + "   ",
-      LOGIN_CRED.password,
-      messages.success
-    );
-    isLoggedIn = true;
-  });
+//   it("should disable login button when username is whitespace only", () => {
+//     loginUnsuccessful("    ", LOGIN_CRED.password, messages.wrongUsername);
+//     isLoggedIn = false;
+//   });
+//   it("should disable login button when username has leading and trailing whitespace", () => {
+//     loginUnsuccessful(
+//       "   " + LOGIN_CRED.username + "   ",
+//       LOGIN_CRED.password,
+//       messages.success
+//     );
+//     isLoggedIn = true;
+//   });
 
-  it("should toggle password visibility when clicking on show/hide password checkbox", () => {
-    hidepassword();
-  });
-  it('should redirect to Forgot Password page when "Quên mật khẩu" button is clicked', () => {
-    forgotPassword();
-  });
-});
-export { isLoggedIn, login };
+//   it("should toggle password visibility when clicking on show/hide password checkbox", () => {
+//     hidepassword();
+//   });
+//   it('should redirect to Forgot Password page when "Quên mật khẩu" button is clicked', () => {
+//     forgotPassword();
+//   });
+// });
+// export { isLoggedIn, login };
